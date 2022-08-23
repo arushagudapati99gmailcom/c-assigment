07c3a6f51380a60d88e9e70362240b0b249e245e
arushagudapati99gmailcom <105437036+arushagudapati99gmailcom@users.noreply.github.com>
2022-08-23 17:57:11 +0530

    Add files via upload

---
+++ swap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Swap
+    {
+        public static void swap(ref int a, ref int b)
+        {
+            a = a * b;
+            b = a / b;
+            a = a / b;
+        }
+        public static void Main(string[] args)
+        {
+            int a = 10, b = 15;
+            Console.WriteLine($"before Swap a={a} b ={b}");
+            swap(ref a, ref b);
+
+            Console.WriteLine($"after Swap a= {a} b={b}");
+        }
+    }
+}
