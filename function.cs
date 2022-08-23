77eef90e98d3a9584389bfca1bb697de0c8088fe
arushagudapati99gmailcom <105437036+arushagudapati99gmailcom@users.noreply.github.com>
2022-08-23 17:59:29 +0530

    Add files via upload

---
+++ function.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{ 
+    internal class function
+    {
+        public static int Mult(int x,int y,int z)
+        {
+            int mul;
+            mul = x * y * z;
+            return mul;
+            
+            
+        }
+        public static void Main()
+        {
+            int x, y, z;
+            Console.WriteLine("enter x number ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter y number ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter z number ");
+            z = Convert.ToInt32(Console.ReadLine());
+            int res = Mult(x, y, z);
+            Console.WriteLine($"product of three numbers is {res}");
+        }
+        
+
+        }
+    }
+
